9ea5b3717b1ad7e8e83f5feee793d6043bfd6ca7
Clark Lawlor <clark.lawlor.419@gmail.com>
2016-11-29 08:02:40 -0700

    Update Test1.js

--- Test1.js
+++ Test1.js
@@ -1,1 +1,9 @@
-
+function test1(){
+  var input     = "haveaniceday";
+  var expected  = "hae and via ecy";
+  var actual    = decodeMessage(input);
+  if (actual == expected)
+    alert("Pass!");
+  else
+    alert("Fail");
+}
